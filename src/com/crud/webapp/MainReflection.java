b46a46e7535a9c7d3ed64ed24eafdd7f32c4df93
Your Name <artem-sadovskij@yandex.ru>
2020-12-28 23:43:17 +0300

    HW_4_reflection

--- src/com/crud/webapp/MainReflection.java
+++ src/com/crud/webapp/MainReflection.java
@@ -3,16 +3,21 @@ package com.crud.webapp;
 import com.crud.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
+        Class<? extends Resume> resumeClass = r.getClass();
+        Field field = resumeClass.getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
+        Method method = resumeClass.getMethod("toString");
+        Object result = method.invoke(r);
         // TODO : invoke r.toString via reflection
-        System.out.println(r);
+        System.out.println(result);
     }
 }
