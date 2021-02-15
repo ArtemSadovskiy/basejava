8dab909ce6410cb4c6cf229a1d3cc7b81c6783de
Your Name <artem-sadovskij@yandex.ru>
2021-02-15 22:50:57 +0300

    HW_08

--- src/com/crud/webapp/model/ListSection.java
+++ src/com/crud/webapp/model/ListSection.java
@@ -1,17 +1,24 @@
 package com.crud.webapp.model;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class ListSection extends Section {
     private final List<String> items;
 
+    public ListSection(String... items) {
+        this(Arrays.asList(items));
+    }
+
     public ListSection(List<String> items) {
         Objects.requireNonNull(items, "items must not be null");
         this.items = items;
     }
 
-    public List<String> getItems() {return items;}
+    public List<String> getItems() {
+        return items;
+    }
 
     @Override
     public String toString() {
