d46477d3d5fd482165c543980296756dd35315c9
Your Name <artem-sadovskij@yandex.ru>
2021-02-25 00:36:46 +0300

    SqlStorage

--- src/com/crud/webapp/exception/StorageException.java
+++ src/com/crud/webapp/exception/StorageException.java
@@ -1,17 +1,22 @@
 package com.crud.webapp.exception;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class StorageException extends RuntimeException {
     private final String uuid;
 
-    public StorageException(String mesage) {
-        this(mesage, null, null);
+    public StorageException(String message) {
+        this(message, null, null);
     }
 
     public StorageException(String message, String uuid) {
         super(message);
         this.uuid = uuid;
+    }
+
+    public StorageException(Exception e) {
+        this(e.getMessage(), e);
     }
 
     public StorageException(String message, Exception e) {
