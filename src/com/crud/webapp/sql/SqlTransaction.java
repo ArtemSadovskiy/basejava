47aa5fb8e14899a0c439e0aa58fb79ffcf363dde
Your Name <artem-sadovskij@yandex.ru>
2021-02-26 15:55:15 +0300

    Transaction_Batch

---
+++ src/com/crud/webapp/sql/SqlTransaction.java
@@ -0,0 +1,8 @@
+package com.crud.webapp.sql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface SqlTransaction<T> {
+    T execute(Connection conn) throws SQLException;
+}
